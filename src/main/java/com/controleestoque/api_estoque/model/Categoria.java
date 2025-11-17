5e5a51fc7e4e0cb475472a639de4b47bfe12418d
Bea-Xavier <bvinguerti@gmail.com>
2025-11-17 10:15:42 -0300

    first commit

---
+++ src/main/java/com/controleestoque/api_estoque/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.controleestoque.api_estoque.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    // --- Relacionamento 1:N (One-To-Many) ---
+    // É o lado '1' do relacionamento. 'mappedBy' aponta para o campo em Produto. 
+    
+    // @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    // private List<Produto> produtos;
+
+    // Construtores, Getters e Setters...
+    public Categoria() {}
+
+    // public Categoria(String nome, List<Produto> produtos) {
+    //     this.nome = nome;
+    //     this.produtos = produtos;
+    // }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    // public List<Produto> geProdutos() { return produtos; }
+    // public void setProdutos(List<Produtos> produtos) { this.produtos = produtos; }
+    
+}
---
+++ src/main/java/com/controleestoque/api_estoque/model/Fornecedor.java
@@ -0,0 +1,46 @@
+package com.controleestoque.api_estoque.model;
+
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_fornecedores")
+public class Fornecedor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    // --- Relacionamento N:M (Many-To-Many) ---
+    // Mapeamento: Lado inverso do relacionamento em Produto.
+    // 'mappedBy' indica que o mapeamento da tabela de junção está na classe Produto.
+    
+    // @ManyToAny(mappedBy = "fornecedores")
+    // private Set<Produto> produtos;
+
+    // Construtores, Getters e Setters...
+    public Fornecedor() {}
+
+    // public Fornecedor(String nome, Set<Produto> produtos) {
+    //     this.nome = nome;
+    //     this.produtos = produtos;
+    // }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    // public List<Produto> geProdutos() { return produtos; }
+    // public void setProdutos(List<Produtos> produtos) { this.produtos = produtos; }
+
+}
